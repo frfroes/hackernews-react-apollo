b735568140d14375312463c3739615079f9bcf1e
Filipe Fróes <filipe.froes@ssp.ba.gov.br>
2018-07-12 11:46:34 -0300

    Creation of Links components and Loading simple queries

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import LinkList from './LinkList'
+
+class App extends Component {
+  render() {
+    return (
+      <LinkList />
+    );
+  }
+}
+
+export default App;
